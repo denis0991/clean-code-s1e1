e5ae66ff7175f0a2f563140ce15bce4200bbc184
Denis Nasonov <brain0991@gmail.com>
2023-04-23 21:39:23 +0500

    refactor(basic-3.6, basic-3.7): add space and semicolon

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var taskInput=document.querySelector(".section-add__wrapper__new-task");//Add a new task.
+var addButton=document.querySelector(".section-add__wrapper__new-task__button");//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
